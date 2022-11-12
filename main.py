3a3cbaa549259830fa317e8e62f8b6e3ea3ccd4c
M.D. Walters <808426@pdsb.net>
2022-11-12 13:51:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ for i in range(1, 10):
     meower.change_page(i)
     for i in range(0, meower.home_len()):
         print(meower.get_post(meower.find_post(i)))
-        f = open(date.strftime("%m-%d-%Y.txt"), "a")
+        f = open(date.strftime("posts/%m-%d-%Y.txt"), "a")
         f.write(f"{meower.get_post(meower.find_post(i))}\n")
         f.close()

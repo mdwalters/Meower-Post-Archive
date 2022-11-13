a672d5b8fefe4419aace94e3d39695506247d30e
M.D. Walters <808426@pdsb.net>
2022-11-12 22:08:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import datetime
 
 date = datetime.datetime.now()
 
-for i in range(1, 10):
+for i in range(1, 30):
     meower.change_page(i)
-    for i in range(0, meower.home_len()):
+    for i in range(0, 25):
         print(meower.get_post(meower.find_post(i)))
         f = open(date.strftime("posts/%m-%d-%Y.txt"), "a")
         f.write(f"{meower.get_post(meower.find_post(i))}\n")

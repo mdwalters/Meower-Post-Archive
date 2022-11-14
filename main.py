3bccaa5647325b3e761ef303e1c3000b315952cf
M.D. Walters <808426@pdsb.net>
2022-11-14 13:36:17 -0500

    100 pages to archive

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import datetime
 
 date = datetime.datetime.now()
 
-for i in range(1, 30):
+for i in range(1, 100):
     meower.change_page(i)
     for i in range(0, 25):
         print(meower.get_post(meower.find_post(i)))
